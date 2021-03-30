7fc874db457cef4a773cc01a2bfc4e3342751121
dujunyan <812313299@qq.com>
2021-03-30 17:50:30 +0800

    后台管理

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 const cors = require('cors');
 const { checkLogin } = require('./utils/jwt');
 
@@ -20,26 +18,11 @@ const knex = require('knex')({
   debug: true
 });
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
 // app.use(logger('dev'));
 app.use(express.urlencoded({extended: true }));
 app.use(express.json());
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors()); // 注入cors模块解决跨域
-// app.all('*', function(req, res, next) {
-//   //设置允许跨域的域名，*代表允许任意域名跨域
-//   res.header("Access-Control-Allow-Origin","*");
-//   //允许的header类型
-//   res.header("Access-Control-Allow-Headers","content-type");
-//   //跨域允许的请求方式 
-//   // res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-//   if (req.method.toLowerCase() == 'options') res.send(200);  //让options尝试请求快速结束
-//   else next();
-// })
 app.use(checkLogin); // 校验token
 app.use('/weixin', require('./routes/weixin').getToken(knex));
 app.post('/api/login', require('./routes/login').login(knex));
