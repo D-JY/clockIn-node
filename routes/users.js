32310a21876c16e53b212edec190453abab826fe
dujunyan <812313299@qq.com>
2021-04-12 18:01:02 +0800

    后台管理

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { varifyToken } = require('../utils/jwt');
 
-// 获取用户信息
 function user(knex) {
+  // 获取用户信息
   router.get('/user', function(req, res, next) {
     const token = req.get('Authorization');
     const obj = varifyToken(token);
@@ -14,6 +14,19 @@ function user(knex) {
       } else {
         res.json({ success: false, message: '获取不到用户信息' });
       }
+    })
+  })
+  // 获取后台用户列表
+  router.get('/userList', function(req, res, next) {
+    const { name: username, role } = req.query;
+    let where = {};
+    if (username) where['username'] = username;
+    if (role) where['role'] = role;
+    knex('user').select().where(where).then(data => {
+      res.json({ success: true, message: '获取用户列表成功', data: data.map(val => {
+        const { id, username, registerDate, registerTime, role } = val;
+        return { id, username, registerDate, registerTime, role }
+      }) })
     })
   })
   return router;
